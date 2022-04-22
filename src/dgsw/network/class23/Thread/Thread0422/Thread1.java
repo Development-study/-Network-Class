4aa73a7673da44d880131f32c18db622d2961453
Yongbin <rhdydqls1024@naver.com>
2022-04-22 15:27:18 +0900

    Thread

---
+++ src/dgsw/network/class23/Thread/Thread0422/Thread1.java
@@ -0,0 +1,17 @@
+package dgsw.network.class23.Thread.Thread0422;
+
+public class Thread1 extends Thread{ //implements Runnable
+
+    //작업구현
+    public void run() {
+        for (int i=0; i < 500; i++) {
+            System.out.printf("t1 Thread : %d\n", i);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/dgsw/network/class23/Thread/Thread0422/ThreadMain.java
@@ -0,0 +1,13 @@
+package dgsw.network.class23.Thread.Thread0422;
+
+import dgsw.network.class23.Thread.Thread1.Thread1;
+
+public class ThreadMain {
+
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+
+
+    }
+
+}
